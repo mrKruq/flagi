1367c86c995d2119f54ca7a47fe120034ab8c5c7
mrKruq <lukaszkruk32@onet.pl>
2024-10-09 15:16:15 +0200

    Wyodrebnienie napisow

--- app/src/main/java/com/example/flagi/MainActivity.java
+++ app/src/main/java/com/example/flagi/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.flagi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int licznikZlych = 0;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewPolecenie);
+    }
+
+    public void zlaOdpowiedz(View view) {
+        Toast.makeText(this, R.string.blden, Toast.LENGTH_SHORT).show();
+    }
+
+    public void dobraOdpowiedz(View view) {
+        view.setVisibility(View.INVISIBLE);
+        licznikZlych++;
+        if (licznikZlych == 4){
+            textView.setText(R.string.podsumowanie);
+        }
+            ;
     }
 }
